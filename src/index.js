ec175badb5a4727b4edad2ea795084c23e1b41b7
Enrique Munguía <enrique7mc@gmail.com>
2018-03-12 21:03:17 -0700

    Capítulo 3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-const arr = ['JavaScript', 'Node', 'Webpack', "React"];
-console.log(arr);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+
+class Message extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+				<p>{this.props.message}</p>
+        <div id="image"></div>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<Message title="Learn webpack"
+	message="Add webpack-dev-server" />,
+        document.getElementById('react-container'))
+
+        // consol.log("error");
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,40 @@ module.exports = {
 	output: {
 		filename: "bundle.js",
 		path: path.join(__dirname, "dist")
-	}
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+		port: 8080
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [{
+      test: /.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["env", "stage-1", "react"]
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: [
+        {loader: "style-loader"},
+        {loader: "css-loader"}
+      ]
+    }, {
+      test: /\.scss$/,
+      use: [
+        {loader: "style-loader"},
+        {loader: "css-loader"},
+        {loader: "sass-loader"}
+      ]
+    }, {
+      test: /\.jpg$/,
+      use: [
+        {loader: "url-loader"}
+      ]
+    }]
+  }
 }
